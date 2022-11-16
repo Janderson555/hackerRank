edb287ab7d9898dc0f297a7cf97f270d0fe6cd13
Janderson555 <jandinsm@gmail.com>
2022-11-16 19:53:49 -0300

    remove comentarios

--- completeExercicies/diagonalDifference.js
+++ completeExercicies/diagonalDifference.js
@@ -1,10 +1,3 @@
-/*
- * Complete the 'diagonalDifference' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts 2D_INTEGER_ARRAY arr as parameter.
- */
-
 function diagonalDifference(arr) {
   let diagonalA = 0;
   let diagonalB = 0;
--- completeExercicies/plusMinus.js
+++ completeExercicies/plusMinus.js
@@ -1,8 +1,8 @@
 function plusMinus(arr) {
   const arrLen = arr.length;
 
-  let positiveNumbers = 0; // 3
-  let negativeNumbers = 0; // 2
+  let positiveNumbers = 0;
+  let negativeNumbers = 0;
   arr.forEach((e) => {
     if(e < 0) {
       negativeNumbers += 1
@@ -11,12 +11,6 @@ function plusMinus(arr) {
       positiveNumbers += 1
     }
   })
-  // return [
-  //   +(positiveNumbers / arrLen).toFixed(6),
-  //   +(negativeNumbers / arrLen).toFixed(6),
-  //   +((arrLen- (positiveNumbers + negativeNumbers)) / arrLen).toFixed(6)
-  // ]
-
     
     console.log(+(positiveNumbers / arrLen).toFixed(6));
     console.log(+(negativeNumbers / arrLen).toFixed(6));
